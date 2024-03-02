e695c3cffaa9936936e8671fc32acc21cfb7de5e
gauthamsathyan <gauthamsathyan@gmail.com>
2024-03-02 15:49:39 -0500

    Add Poll and Option models

--- polls_app/polls/models.py
+++ polls_app/polls/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Poll(models.Model):
+    question = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question
+
+class Option(models.Model):
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    option_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.option_text
