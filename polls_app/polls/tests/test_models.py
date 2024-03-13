1d527cb1af71502173dd3da0bf0b8b0ac46af836
Rafael <rafael@mileiq.com>
2024-03-12 23:21:22 -0300

    change models reference

--- polls_app/polls/tests/test_models.py
+++ polls_app/polls/tests/test_models.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
-from .models import Poll, Option
+from models import Poll, Option
 from django.utils import timezone
-import datetime
 
 class PollModelTest(TestCase):
     def setUp(self):
