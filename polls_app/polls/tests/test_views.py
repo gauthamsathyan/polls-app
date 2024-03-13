4727da8d912035a1e2448b4966d9872d81be1183
Rafael <rafael@mileiq.com>
2024-03-13 00:15:22 -0300

    test actions

--- polls_app/polls/tests/test_views.py
+++ polls_app/polls/tests/test_views.py
@@ -1,12 +1,12 @@
-from django.test import Client, TestCase
-from django.urls import reverse
+# from django.test import Client, TestCase
+# from django.urls import reverse
 
 
-class TestIndex(TestCase):
-    def setUp(self):
-        self.client = Client()
+# class TestIndex(TestCase):
+#     def setUp(self):
+#         self.client = Client()
 
-    def test_index(self):
-        response = self.client.get(reverse("index"))
-        assert response.status_code == 200
-        self.assertTemplateUsed(response, "polls.html")
+#     def test_index(self):
+#         response = self.client.get(reverse("index"))
+#         assert response.status_code == 200
+#         self.assertTemplateUsed(response, "polls.html")
